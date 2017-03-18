1f0de8e2198b6693ef6b97a639dcf40dac29f7ce
Wu Zhangjin <wuzhangjin@gmail.com>
2017-03-18 22:16:48 +0800

    Add cs630-qemu-lab session
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- recordings/records.js
+++ recordings/records.js
@@ -2,4 +2,4 @@ var VNC_slice_size = '256.0K';
 var VNC_record_player = '/vplayer/';
 var VNC_record_dir = '/recordings';
 
-var VNC_record_data = [['Name', 'Title', 'Size', 'Time', 'Create', 'Author', 'Tags', 'Desc', 'Slices'], ['linux-lab.zb64', 'Linux Lab Usage', '937.2K', '00:01:02', '1489241775.0445454', 'Wu Zhangjin <wuzhangjin@gmail.com>', 'Linux, Linux Lab', 'Linux Lab aims to create a quick Linux learning and development environment, this video demonstrates how to use it!', 5]];
+var VNC_record_data = [['Name', 'Title', 'Size', 'Time', 'Create', 'Author', 'Tags', 'Desc', 'Slices'], ['cs630-qemu-lab.zb64', 'CS630 Qemu Lab Usage', '10.6M', '00:03:33', '1489850480.335766', 'Wu Zhangjin <wuzhangjin@gmail.com>', 'CS630, X86, Assembly, Linux', 'Learn the Linux/X86 Assembly Course (CS630) with CS630 Qemu Lab', 45], ['linux-lab.zb64', 'Linux Lab Usage', '937.2K', '00:01:02', '1489241775.0445454', 'Wu Zhangjin <wuzhangjin@gmail.com>', 'Linux, Linux Lab', 'Linux Lab aims to create a quick Linux learning and development environment, this video demonstrates how to use it!', 1]];
